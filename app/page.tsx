48796b64d8b5e4b576599302bc72d21f66bf8fdf
John Soto <soto.ja415@gmail.com>
2023-12-15 00:51:17 -0500

    Refactor: Replaced with welcome

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      yurrrr!
-      <Button>Sign up</Button>
+      <h2>Welcome</h2>
     </div>
   );
 }
