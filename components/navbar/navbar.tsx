1ce99e7d2ffeab0a56618f3f7b0a14d9a8292540
John Soto <soto.ja415@gmail.com>
2023-12-14 23:53:19 -0500

    Feat: Added Navabr component

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { ModeToggle } from "../ui/mode-toggle";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-      <ModeToggle />
-    </div>
+    <nav className="h-18 border-b p-4">
+      <div className="px-10 flex items-center justify-between">
+        <Link href="/">LOGO</Link>
+        <div className="flex text-lg">
+          <Link href="/">Home</Link>
+        </div>
+        <ModeToggle />
+      </div>
+    </nav>
   );
 };
 
