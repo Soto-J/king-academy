112b955ba0abeef9a5a1349ca4b623f6060598e4
John Soto <soto.ja415@gmail.com>
2023-12-15 01:00:15 -0500

    Style: put gap between login button and menu toggle

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -9,14 +9,14 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 const Navbar = () => {
   return (
     <nav className="h-18 border-b p-4">
-      <div className="flex items-center justify-between md:px-10">
+      <div className="flex items-center justify-between sm:px-10">
         <Link href="/">LOGO</Link>
 
         <div className="hidden text-lg md:flex">
           <Link href="/">Home</Link>
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex gap-6">
           <div className="flex gap-2">
             <Button>
               <Link href="/">Sign up</Link>
