2025e2d9451ced57ae1b0a8f5b325c9e9a8d70ec
John Soto <soto.ja415@gmail.com>
2023-12-15 00:54:55 -0500

    Feat: Added mobile dropdown to nav and sign in, sign up buttons

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,16 +1,50 @@
-import React from "react";
-import { ModeToggle } from "../ui/mode-toggle";
 import Link from "next/link";
+
+import { Menu } from "lucide-react";
+
+import { ModeToggle } from "@/components/ui/mode-toggle";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const Navbar = () => {
   return (
     <nav className="h-18 border-b p-4">
-      <div className="px-10 flex items-center justify-between">
+      <div className="flex items-center justify-between md:px-10">
         <Link href="/">LOGO</Link>
-        <div className="flex text-lg">
+
+        <div className="hidden text-lg md:flex">
           <Link href="/">Home</Link>
         </div>
-        <ModeToggle />
+
+        <div className="flex gap-2">
+          <div className="flex gap-2">
+            <Button>
+              <Link href="/">Sign up</Link>
+            </Button>
+            <Button variant="outline">
+              <Link href="/">Log in</Link>
+            </Button>
+
+            <div className="hidden md:block">
+              <ModeToggle />
+            </div>
+          </div>
+
+          <Sheet>
+            <SheetTrigger className="md:hidden" asChild>
+              <Button variant="outline" size="icon">
+                <Menu />
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent side="left">
+              <Link href="/">Home</Link>
+              <div className="flex h-full items-end justify-end pb-4">
+                <ModeToggle />
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
       </div>
     </nav>
   );
