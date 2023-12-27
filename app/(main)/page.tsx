346f435c1cc400de0374916472bca6724fb2888b
John Soto <soto.ja415@gmail.com>
2023-12-26 22:22:45 -0500

    Refactor: Relocated clerk organization folder to root

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,9 +3,7 @@
 import { useAuth } from "@clerk/nextjs";
 
 export default function Home() {
-  const auth = useAuth();
-  console.log(auth);
-  
+
   return (
     <div className="flex flex-col items-center justify-between p-24">
       <h2>Welcome</h2>
