9af845142a6c39bcf256d25e33b9ddc2c27c02d5
John Soto <soto.ja415@gmail.com>
2023-12-26 21:51:24 -0500

    Refactor: Moved navbar to global components folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
-import Navbar from "@/app/(main)/_components/navbar";
+import Navbar from "@/components/navbar/navbar";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
