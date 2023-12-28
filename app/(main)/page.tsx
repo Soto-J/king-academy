f2ecad02b5738d1d5047e997e837a12c0cadb02d
John Soto <soto.ja415@gmail.com>
2023-12-27 23:40:58 -0500

    Feat: Redirect user if no urser profile

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,18 @@
-"use client";
+import { auth } from "@clerk/nextjs";
+import { getUser } from "@/app/actions/getUser";
+import { redirect } from "next/navigation";
 
-import { useAuth } from "@clerk/nextjs";
+export default async function Home() {
+  const { sessionId, userId } = auth();
 
-export default function Home() {
+  if (sessionId) {
+    const user = await getUser(userId);
 
+    if (!user) {
+      return redirect("/profile");
+    }
+  }
+  
   return (
     <div className="flex flex-col items-center justify-between p-24">
       <h2>Welcome</h2>
