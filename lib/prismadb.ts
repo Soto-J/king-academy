7c974516c3cddafb34600be7b095dc21e82290b7
John Soto <soto.ja415@gmail.com>
2023-12-29 01:39:48 -0500

    Feat: Added prisma accelerate

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,11 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const client =
-  globalThis.prisma || new PrismaClient();
+const client = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = client;
