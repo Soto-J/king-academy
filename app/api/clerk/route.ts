91bcb2bf6873bc0ac2c2ef54454af018aaf04bf6
John Soto <soto.ja415@gmail.com>
2023-12-29 17:19:16 -0500

    Refactor: Relocate file

--- app/api/clerk/route.ts
+++
@@ -1,8 +0,0 @@
-import { WebhookEvent } from "@clerk/nextjs/server";
-
-export async function POST(req: Request) {
-  const payload: WebhookEvent = await req.json();
-
-  console.log({ payload });
-  return Response.json({ message: "Received" });
-}
