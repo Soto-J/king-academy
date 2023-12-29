263a3471e160f0bf5b023720ddb033be649d0f3b
John Soto <soto.ja415@gmail.com>
2023-12-28 22:54:36 -0500

    Style: Gave header semi-bold

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -3,7 +3,9 @@ import ProfileForm from "./_component/profile-form";
 const ProfilePage = () => {
   return (
     <div>
-      <h2 className="text-center text-4xl">Complete your profile</h2>
+      <h2 className="text-center text-4xl font-semibold">
+        Complete your profile
+      </h2>
 
       <ProfileForm />
     </div>
