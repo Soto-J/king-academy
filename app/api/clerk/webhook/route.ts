93dd5b1c87cbe7e0796659b3c1af2aa9bb8d75d5
John Soto <soto.ja415@gmail.com>
2023-12-30 16:42:20 -0500

    Feat: Set up boiler plate for webhook

--- app/api/clerk/webhook/route.ts
+++ app/api/clerk/webhook/route.ts
@@ -1,8 +1,52 @@
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
+
+import { Webhook } from "svix";
 
 export async function POST(req: Request) {
-  const payload: WebhookEvent = await req.json();
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-  console.log({ payload });
+  if (!WEBHOOK_SECRET) {
+    return new Response("Error occured -- no webhook secret", {
+      status: 400,
+    });
+  }
+
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
+  }
+
+  const svixHeaders = {
+    "svix-id": svix_id,
+    "svix-timestamp": svix_timestamp,
+    "svix-signature": svix_signature,
+  };
+
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, svixHeaders) as WebhookEvent;
+  } catch (error) {
+    console.error("Error verifying webhook:", error);
+    return new Response("Erorr occured", { status: 400 });
+  }
+
+  return new Response("Received", { status: 200 });
+}
+
+export async function GET() {
   return Response.json({ message: "Received" });
 }
