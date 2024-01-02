a859f2358137fca43b6a8e43209795986606f235
John Soto <soto.ja415@gmail.com>
2024-01-02 02:52:37 -0500

    Fix: Removed unused code

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,26 +1,4 @@
-import { auth, currentUser } from "@clerk/nextjs";
-import { getUser } from "@/app/actions/getUser";
-import { redirect } from "next/navigation";
-
 export default async function Home() {
-  const { sessionId, userId } = auth();
-  // const current = await currentUser();
-
-  // const test = auth();
-
-  // console.log({ test });
-  // console.log({ current });
-  // console.log(current?.emailAddresses);
-  // console.log(current?.phoneNumbers[0].phoneNumber);
-
-  if (sessionId) {
-    const user = await getUser(userId);
-
-    if (!user) {
-      return redirect("/profile");
-    }
-  }
-
   return (
     <div className="flex flex-col items-center justify-between p-24">
       <h2>Welcome</h2>
