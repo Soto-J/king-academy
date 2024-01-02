8ebcbab084b2d697a367e81dddf800542e44a126
John Soto <soto.ja415@gmail.com>
2024-01-02 02:04:24 -0500

    Feat: Added delete logic

--- app/actions/deleteUser.ts
+++ app/actions/deleteUser.ts
@@ -2,7 +2,17 @@ import prisma from "@/lib/prismadb";
 
 export async function deleteUser(userId?: string) {
   try {
-    return null;
+    if (!userId) {
+      return null;
+    }
+
+    const user = await prisma.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+
+    return { message: "User deleted successfully", user };
   } catch (error) {
     console.error("Delete User:", error);
     return null;
