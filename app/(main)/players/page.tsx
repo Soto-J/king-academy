029be8001648f66ee8113e7c16651a0cf45fdcaf
John Soto <soto.ja415@gmail.com>
2024-01-02 14:58:16 -0500

    Chore: Comment out edit profile button

--- app/(main)/players/page.tsx
+++ app/(main)/players/page.tsx
@@ -7,11 +7,10 @@ const PlayerPage = async () => {
 
   return (
     <div>
-      <h1 className="text-center text-3xl font-bold">Players</h1>
-
+      <h1 className="mb-12 text-center text-3xl font-bold">Players</h1>
 
       <PlayersList players={players} />
-      <Button>Edit Profile</Button>
+      {/* <Button>Edit Profile</Button> */}
     </div>
   );
 };
