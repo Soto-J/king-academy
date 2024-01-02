57fec365c7bc5ed573c4a0665a92379a92d1066f
John Soto <soto.ja415@gmail.com>
2024-01-01 20:03:19 -0500

    Refactor: Removed prisma accelerate

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,28 +1,13 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate());
-};
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+const client = globalThis.prisma || new PrismaClient();
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = client;
+}
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-export default prisma;
-
-// declare global {
-//   var prisma: PrismaClient | undefined;
-// }
-
-// const client = globalThis.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") {
-//   globalThis.prisma = client;
-// }
-
-// export default client;
+export default client;
