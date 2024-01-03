58620de836e242eab09942ce6ef4bfe461affece
John Soto <soto.ja415@gmail.com>
2024-01-02 23:39:48 -0500

    Style: Added instagram and facebook icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Facebook, Instagram } from "lucide-react";
+import Link from "next/link";
 
 const Footer = () => {
-  return <footer className="border-t p-4">Footer</footer>;
+  return (
+    <footer className="fixed bottom-0 left-0 flex w-full border-t p-4">
+      <div className="ml-auto flex gap-x-6 sm:px-10">
+        <Link href="/">
+          <Instagram className="h-6 w-6" />
+        </Link>
+        <Link href="/">
+          <Facebook className="h-6 w-6" />
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
