26ebff5fdfa68e74dc4ab4c8c0e1ee372bf68a00
John Soto <soto.ja415@gmail.com>
2024-01-02 23:45:05 -0500

    Refactor: Made nav links into and array

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -7,21 +7,23 @@ import MobileMenu from "./mobile-menu";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 
 const Navbar = () => {
-  const NAV_LINKS = (
-    <>
-      <Link href="/">Home</Link>
-      <Link href="/players">Players</Link>
-      <Link href="/about-us">About Us</Link>
-      <Link href="/contact">Contact</Link>
-    </>
-  );
+  const NAV_LINKS = [
+    <Link href="/">Home</Link>,
+    <Link href="/players">Players</Link>,
+    <Link href="/about-us">About Us</Link>,
+    <Link href="/contact">Contact</Link>,
+  ];
 
   return (
     <nav className="h-18 border-b p-4 shadow-lg">
       <div className="flex items-center justify-between sm:px-10">
         <Link href="/">LOGO</Link>
 
-        <div className="hidden gap-x-4 text-lg md:flex">{NAV_LINKS}</div>
+        <ul className="hidden gap-x-4 text-lg md:flex">
+          {NAV_LINKS.map((link, i) => (
+            <li key={i}>{link}</li>
+          ))}
+        </ul>
 
         <div className="flex gap-6">
           <div className="flex items-center gap-2 gap-x-4">
