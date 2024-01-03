c25e3608317515eed0ee0e11807608bc5ded131e
John Soto <soto.ja415@gmail.com>
2024-01-02 23:35:37 -0500

    Refactor: Removed styles and edit button

--- app/(main)/players/page.tsx
+++ app/(main)/players/page.tsx
@@ -6,11 +6,10 @@ const PlayerPage = async () => {
   const players = await getAllPlayers();
 
   return (
-    <div>
+    <div className="">
       <h1 className="mb-12 text-center text-3xl font-bold">Players</h1>
 
       <PlayersList players={players} />
-      {/* <Button>Edit Profile</Button> */}
     </div>
   );
 };
