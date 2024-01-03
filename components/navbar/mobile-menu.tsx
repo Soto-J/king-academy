d4256c09e78fb080dcd2e0a9545b4473f01fc149
John Soto <soto.ja415@gmail.com>
2024-01-02 23:44:03 -0500

    Feat: navbar will close when choosing a link

--- components/navbar/mobile-menu.tsx
+++ components/navbar/mobile-menu.tsx
@@ -1,11 +1,16 @@
 import { Menu } from "lucide-react";
 
 import { ModeToggle } from "@/components/ui/mode-toggle";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 
 type MobileMenuProps = {
-  NAV_LINKS: JSX.Element;
+  NAV_LINKS: JSX.Element[];
 };
 
 const MobileMenu = ({ NAV_LINKS }: MobileMenuProps) => {
@@ -18,7 +23,13 @@ const MobileMenu = ({ NAV_LINKS }: MobileMenuProps) => {
       </SheetTrigger>
 
       <SheetContent side="left">
-        <div className="flex flex-col gap-3">{NAV_LINKS}</div>
+        <ul className="flex flex-col gap-3">
+          {NAV_LINKS.map((link, i) => (
+            <li key={i}>
+              <SheetClose asChild>{link}</SheetClose>
+            </li>
+          ))}
+        </ul>
         <div className="flex h-[80%] items-end justify-end pb-4">
           <ModeToggle />
         </div>
