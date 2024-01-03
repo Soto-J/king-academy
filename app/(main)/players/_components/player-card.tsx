d0f8f463e897ed695719a9c011a7b78c90b9c9e7
John Soto <soto.ja415@gmail.com>
2024-01-02 23:39:03 -0500

    Refactor: Created player card

---
+++ app/(main)/players/_components/player-card.tsx
@@ -0,0 +1,45 @@
+import {
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { User } from "@prisma/client";
+import React from "react";
+
+type PlayerCardProps = {
+  player: User;
+};
+
+const PlayerCard = ({ player }: PlayerCardProps) => {
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>{`${player.firstName} ${player.lastName}`}</DialogTitle>
+        <DialogDescription>Player Card</DialogDescription>
+      </DialogHeader>
+
+      <div className="space-y-3 py-4">
+        <div>School:</div>
+        <div>Age:</div>
+        <div>Address:</div>
+        <div>
+          Email:{" "}
+          {player.emails.map((email) => (
+            <div key={player.id}>{email}</div>
+          ))}
+        </div>
+        <div>Active:</div>
+        <div>Batting:</div>
+        <div>Throwing:</div>
+        <div>
+          Positions:
+          {player.positions.map((position) => (
+            <div key={player.id}>{position}</div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PlayerCard;
