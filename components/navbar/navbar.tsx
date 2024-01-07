7d38b61f59b51f4456994e599133a854e76b0687
John Soto <soto.ja415@gmail.com>
2024-01-07 18:01:54 -0500

    Style: Added logo to navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,10 +5,9 @@ import MobileMenu from "./mobile-menu";
 
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { auth } from "@/auth";
+import Logo from "../logo";
 
 const Navbar = async () => {
-  
-
   const NAV_LINKS = [
     <Link href="/">Home</Link>,
     <Link href="/players">Players</Link>,
@@ -19,7 +18,7 @@ const Navbar = async () => {
   return (
     <nav className="h-18 border-b p-4 shadow-lg">
       <div className="flex items-center justify-between sm:px-10">
-        <Link href="/">LOGO</Link>
+        <Logo />
 
         <ul className="hidden gap-x-4 text-lg md:flex">
           {NAV_LINKS.map((link, i) => (
