0b72e89fb48b19ab27273e99570ce3a87bf45f2d
John Soto <soto.ja415@gmail.com>
2024-01-08 00:41:31 -0500

    Feat: Added signIn onClick

--- app/(authentication)/auth/_components/form/social.tsx
+++ app/(authentication)/auth/_components/form/social.tsx
@@ -1,18 +1,29 @@
 "use client";
 
+import { signIn } from "next-auth/react";
+
 import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
+
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const Social = () => {
+  const onSignIn = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
-    <div className="flex items-center w-full gap-x-2">
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
+    <div className="flex w-full items-center gap-x-2">
+      <Button
+        size="lg"
+        className="w-full"
+        variant="outline"
+        onClick={() => onSignIn("google")}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      {/* <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FaGithub className="h-5 w-5" />
-      </Button> */}
     </div>
   );
 };
