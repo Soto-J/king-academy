07b8ef115c57dc59173f4fb998fde79b84e3d60c
John Soto <soto.ja415@gmail.com>
2024-01-08 00:42:33 -0500

    Feat: Added google provider

--- auth.config.ts
+++ auth.config.ts
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 
 import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
 
 import { LoginSchema } from "@/schemas";
 
@@ -9,6 +10,10 @@ import { getUserByEmail } from "./data/user";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
