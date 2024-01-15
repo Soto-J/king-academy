de3b930335427322e820e0394071bc71e0a004d6
John Soto <soto.ja415@gmail.com>
2024-01-15 03:09:23 -0500

    Refactor: Made footer static

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 flex w-full border-t p-4">
+    <footer className="static bottom-0 left-0 flex w-full border-t p-4">
       <div className="ml-auto flex gap-x-6 sm:px-10">
         <Link href="/">
           <Instagram className="h-6 w-6" />
