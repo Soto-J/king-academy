07695472fedac0b5795c842549a7b64975693b07
John Soto <soto.ja415@gmail.com>
2024-01-17 01:00:40 -0500

    Style: update style

--- app/(main)/players/_components/player/player-card.tsx
+++ app/(main)/players/_components/player/player-card.tsx
@@ -5,8 +5,13 @@ import {
 } from "@/components/ui/dialog";
 import { UserAvatar } from "@/components/user-avatar";
 import { type UserWithProfileAndAddress } from "@/data/user";
-import { PlayerAddress } from "./player-address";
 
+import { Mail, School } from "lucide-react";
+import { FaBaseballBatBall } from "react-icons/fa6";
+import { GiThrowingBall } from "react-icons/gi";
+import { CiMail } from "react-icons/ci";
+import { FaSchool } from "react-icons/fa";
+import { FaBirthdayCake } from "react-icons/fa";
 type PlayerCardProps = {
   player: UserWithProfileAndAddress;
 };
@@ -30,34 +35,41 @@ export const PlayerCard = ({ player }: PlayerCardProps) => {
         <DialogDescription>{player.username}</DialogDescription>
       </DialogHeader>
 
-      <div className="space-y-3 py-4 [&>div>p]:font-light [&>div>span]:font-bold [&>div>span]:text-primary [&>div]:flex [&>div]:gap-x-2">
+      <div className="space-y-5 py-4 [&>div>p]:font-light [&>div>span]:font-bold [&>div>span]:text-primary [&>div]:flex [&>div]:items-center [&>div]:gap-x-4">
         <div>
-          <span>Active:</span>
-          <p>{player.profile.isActive}</p>
-        </div>
-
-        <div>
-          <span className="font-bold text-primary">School:</span>
+          <span>
+            <FaSchool className="h-6 w-6" />
+          </span>
           <p>{player.profile.school}</p>
         </div>
 
         <div>
-          <span className="font-bold text-primary">Age:</span>
-          <p>{player.profile.age}</p>
+          <span className="font-bold text-primary">
+            <FaBirthdayCake className="h-6 w-6" />
+          </span>
+          <p>
+            {player.profile.age}, {player.profile.dateOfBirth}
+          </p>
         </div>
 
         <div>
-          <span className="font-bold text-primary">Email:</span>
+          <span>
+            <CiMail className="h-6 w-6" />
+          </span>
           <p>{player.email}</p>
         </div>
 
         <div>
-          <span className="font-bold text-primary">Batting:</span>
+          <span>
+            <FaBaseballBatBall className="h-6 w-6" />
+          </span>
           <p className="capitalize">{player.profile.batting.toLowerCase()}</p>
         </div>
 
         <div>
-          <span className="font-bold text-primary">Throwing:</span>
+          <span>
+            <GiThrowingBall className="h-6 w-6" />
+          </span>
           <p className="capitalize">
             {player.profile.throwing.toLocaleLowerCase()}
           </p>
