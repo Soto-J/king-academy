744541f8c729f10b84ddee40d6d86b24cdcee5ca
John Soto <soto.ja415@gmail.com>
2024-07-12 02:32:50 -0400

    Fixed errors on user create

--- actions/createUser.ts
+++ actions/createUser.ts
@@ -1,23 +1,28 @@
 import { type UserJSON } from "@clerk/nextjs/server";
 
 import prisma from "@/lib/prismadb";
-import { Role } from "@prisma/client";
 
 export async function createUser(user: UserJSON) {
   try {
-    const { id, first_name, last_name, phone_numbers, email_addresses } = user;
-
-    const phoneNumbers = phone_numbers.map((number) => number.phone_number);
-    const emails = email_addresses.map((email) => email.email_address);
+    const {
+      id,
+      first_name,
+      last_name,
+      phone_numbers,
+      email_addresses,
+      image_url,
+      username,
+    } = user;
 
     const newUser = await prisma.user.create({
       data: {
-        id,
+        externalId: id,
+        username: username || "",
         firstName: first_name,
         lastName: last_name,
-        phoneNumbers,
-        emails,
-        role: Role.MEMBER,
+        phoneNumber: phone_numbers[0].phone_number,
+        email: email_addresses[0].email_address,
+        imageUrl: image_url,
       },
     });
 
