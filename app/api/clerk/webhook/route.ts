65723e646cd0601fcb2e5624f415681d91f2691f
John Soto <soto.ja415@gmail.com>
2024-07-12 02:19:52 -0400

    Changed catch response

--- app/api/clerk/webhook/route.ts
+++ app/api/clerk/webhook/route.ts
@@ -65,6 +65,6 @@ export async function GET() {
     console.log("Api Test");
     return Response.json({ message: "Api Test" });
   } catch (error) {
-    return Response.error;
+    return Response.error();
   }
 }
