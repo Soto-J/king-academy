c065d2ee553723ccdf0c6b7caffe77801e9ee797
John Soto <soto.ja415@gmail.com>
2024-07-16 22:26:43 -0400

    update

--- app/(main)/(home)/page.tsx
+++ app/(main)/(home)/page.tsx
@@ -2,14 +2,14 @@ import ImageSlider from "./_components/image-slides";
 
 export default async function Home() {
   return (
-    <div className="mx-auto w-[95%] max-w-2xl text-center">
+    <div className="mx-auto w-[95%] max-w-2xl text-center tracking-wide ">
       <h1 className="pb-12 text-2xl font-bold sm:text-4xl">
         Welcome to Baseball King Academy!
       </h1>
 
       <ImageSlider />
 
-      <p className="py-12 leading-8 sm:text-xl">
+      <p className="py-12 sm:text-xl sm:leading-8">
         We are thrilled to provide a fun and engaging environment for young
         athletes to learn, play, and grow in the sport of baseball. Our team is
         dedicated to fostering teamwork, sportsmanship, and skill development in
