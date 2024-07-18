ca1fa29d5bdc15d764595a430c22dd19aeafcb07
Michael Nunez <mnunez903@gmail.com>
2024-07-18 11:47:37 -0400

    Update README, routing, and .gitignore

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -8,7 +8,7 @@ import NavbarActions from "./navbar-buttons";
 const Navbar = () => {
   const NAV_LINKS = [
     { routeName: "Home", href: "/" },
-    { routeName: "Galary", href: "/galary" },
+    { routeName: "Gallery", href: "/gallery" },
     { routeName: "About Us", href: "/about-us" },
     { routeName: "Contact", href: "/contact" },
     { routeName: "Players", href: "/players" },
